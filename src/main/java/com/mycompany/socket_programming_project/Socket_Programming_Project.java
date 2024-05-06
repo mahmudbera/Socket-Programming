a3a33ba98c067f7f553f9d6b0f265db027499d77
Mahmud Bera Karagöz <mberakrgoz@gmail.com>
2024-05-06 21:38:16 +0300

    c

---
+++ src/main/java/com/mycompany/socket_programming_project/Socket_Programming_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.socket_programming_project;
+
+/**
+ *
+ * @author mbera
+ */
+public class Socket_Programming_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
